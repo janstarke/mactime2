72d36d2c635cdcf2f7241f48df7b2a3c4c251b77
Jan Starke <jan.starke@outofbed.org>
2021-11-21 14:57:22 +0100

    support for csv and txt

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,10 @@ fn main() -> Result<()> {
                 .required(false)
                 .multiple(false)
                 .takes_value(true),
+        ).arg(
+            Arg::with_name("CSV_FORMAT")
+                .short("d")
+                .help("output as CSV instead of TXT")
         );
 
     let matches = app.get_matches();
@@ -42,7 +46,13 @@ fn main() -> Result<()> {
     let _ = reader.join();
     let _ = decoder.join();
     match sorter.join() {
-        Ok(entries) => display_csv(entries),
+        Ok(entries) => {
+            if matches.is_present("CSV_FORMAT") {
+                display_csv(entries)
+            } else {
+                display_txt(entries)
+            }
+        }
         Err(why) => {
             log::error!("{:?}", why);
         }
@@ -50,21 +60,47 @@ fn main() -> Result<()> {
     Ok(())
 }
 
+fn format_date(unix_ts: i64) -> String {
+    let timestamp = NaiveDateTime::from_timestamp(unix_ts, 0);
+    timestamp.format("%a %b %d %Y %T").to_string()
+}
+
 fn display_csv(entries: BTreeMap<i64, BTreeSet<ListEntry>>) {
     println!("Date,Size,Type,Mode,UID,GID,Meta,File Name");
     for (ts, entries_at_ts) in entries.iter() {
-        let timestamp = NaiveDateTime::from_timestamp(*ts, 0);
-        let timestamp = timestamp.format("%a %b %d %Y %T");
+        let timestamp = format_date(*ts);
         for line in entries_at_ts {
             println!(
-                "{},{},{},{},0,0,{},\"{}\"",
+                "{},{},{},{},{},{},{},\"{}\"",
                 timestamp,
                 line.line.get_size(),
                 line.flags,
                 line.line.get_mode(),
+                line.line.get_uid(),
+                line.line.get_gid(),
                 line.line.get_inode(),
                 line.line.get_name()
             );
+        }
+    }
+}
+
+fn display_txt(entries: BTreeMap<i64, BTreeSet<ListEntry>>) {
+    for (ts, entries_at_ts) in entries.iter() {
+        let mut timestamp = format_date(*ts);
+        for line in entries_at_ts {
+            println!(
+                "{} {:>8} {} {:<12} {:<7} {:<7} {} {}",
+                timestamp,
+                line.line.get_size(),
+                line.flags,
+                line.line.get_mode(),
+                line.line.get_uid(),
+                line.line.get_gid(),
+                line.line.get_inode(),
+                line.line.get_name()
+            );
+            timestamp = "                        ".to_owned();
         }
     }
 }
